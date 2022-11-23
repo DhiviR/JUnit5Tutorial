e0ef8742952a50d9b19f77de19ae6bc8642c0991
DhiviR <rubandhivi8@gmail.com>
2022-11-23 11:02:59 +0530

    First commit

---
+++ src/main/java/com/dhivi/demo/test/junit5/DemoTestJunit5Application.java
@@ -0,0 +1,13 @@
+package com.dhivi.demo.test.junit5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTestJunit5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTestJunit5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhivi/demo/test/junit5/DemoTestJunit5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhivi.demo.test.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestJunit5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
